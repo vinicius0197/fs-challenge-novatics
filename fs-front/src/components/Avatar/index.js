2db3eaa5fc9139c81d8ebe12c6564553a7848bec
Vinicius Costa <viniciuscosta0197@gmail.com>
2020-05-22 16:01:01 -0300

    adding basic style for grid

--- fs-front/src/components/Avatar/index.js
+++ fs-front/src/components/Avatar/index.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Faker from "faker";
+
+import { ReactComponent as LearnIcon } from "../../assets/closed-book.svg";
+import { ReactComponent as ClapIcon } from "../../assets/clap.svg";
+import { ReactComponent as GratefulIcon } from "../../assets/pray.svg";
 
 import "./index.css";
 
 const Avatar = (props) => {
+  const [selected, setSelected] = useState(false);
   const getAvatar = () => {
     return Faker.image.avatar();
   };
 
+  const updateKudos = (userId, kudoType) => {
+    // TODO: send action with userId and kudoType
+  };
+
   return (
-    <img
-      className="avatar"
-      key={props.user.userName}
-      src={getAvatar()}
-      alt={props.user.userName}
-    />
+    <div
+      className="avatar_container"
+      onMouseEnter={() => setSelected(true)}
+      onMouseLeave={() => setSelected(false)}
+    >
+      <img
+        className={selected ? "avatar-selected" : "avatar"}
+        key={props.user.id}
+        src="https://s3.amazonaws.com/uifaces/faces/twitter/suribbles/128.jpg"
+        alt={props.user.userName}
+      />
+      <div className={selected ? "middle_selected" : "middle"}>
+        <LearnIcon
+          className="icon"
+          onClick={() => updateKudos(props.user.id, "learn")}
+        />
+        <ClapIcon
+          className="icon"
+          onClick={() => updateKudos(props.user.id, "clap")}
+        />
+        <GratefulIcon
+          className="icon"
+          onClick={() => updateKudos(props.user.id, "grateful")}
+        />
+      </div>
+    </div>
   );
 };
 
