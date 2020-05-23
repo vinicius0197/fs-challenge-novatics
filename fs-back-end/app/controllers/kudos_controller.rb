48ae7fca459a1396d53638dc2380284af1bd77d4
Vinicius Costa <viniciuscosta0197@gmail.com>
2020-05-23 15:35:16 -0300

    creating controller for kudos

---
+++ fs-back-end/app/controllers/kudos_controller.rb
@@ -0,0 +1,16 @@
+class KudosController < ApplicationController
+  def create
+    kudo = Kudo.create(kudo_params)
+    if kudo.valid?
+      render json: { success: 'Kudo was created' }, status: :created
+    else
+      render json: { errors: 'Request invalid' }, status: :not_acceptable
+    end
+  end
+
+
+  private
+  def kudo_params
+    params.require(:kudo).permit(:description, :sender_id, :recipient_id)
+  end
+end
